327665932d92c2b2a8a227bec3724688771d730e
Zachary Voase <z@zacharyvoase.com>
2011-12-16 10:19:07 +0000

    Added packaging skeleton.

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distribute_setup import use_setuptools
+use_setuptools()
+
+from setuptools import setup, find_packages
+import os.path as p
+
+VERSION = open(p.join(p.dirname(p.abspath(__file__)), 'VERSION')).read().strip()
+
+setup(
+    name='zrpc',
+    version=VERSION,
+    description='Simple ZeroMQ-based RPC.',
+    author='Zachary Voase',
+    author_email='z@dvxhouse.com',
+    url='http://github.com/zacharyvoase/zrpc',
+    packages=find_packages(where='lib'),
+    package_dir={'': 'lib'},
+    install_requires=[
+        'pyzmq==2.1.10',
+    ],
+)
