7623498928bdbcf75788007da002041c5898ba66
Zachary Voase <z@zacharyvoase.com>
2011-12-16 17:32:02 +0000

    Fixed zrpc.exceptions documentation.

--- lib/zrpc/exceptions.py
+++ lib/zrpc/exceptions.py
@@ -1,5 +1,12 @@
+"""
+Common exceptions throughout the ZRPC server.
+
+Note that :class:`zrpc.client.Error` doesn't live here; this module houses
+server-specific exceptions.
+"""
+
 class ZRPCError(Exception):
-    """A generic error somewhere in the ZRPC error."""
+    """A generic error somewhere in the ZRPC server."""
     pass
 
 
