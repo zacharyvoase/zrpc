3cbeccc6a00d13bcf723e4d07af1402a595ddcf4
Zachary Voase <z@zacharyvoase.com>
2011-12-18 21:06:47 +0000

    Added LoadBalancer, with tests.

---
+++ lib/zrpc/loadbal.py
@@ -0,0 +1,69 @@
+import logbook
+
+import zmq
+
+
+logger = logbook.Logger('zrpc.loadbal')
+
+
+class LoadBalancer(object):
+
+    """
+    Load balance to multiple ZRPC servers using a ``zmq.QUEUE`` device.
+
+    There are two ways to configure this load balancer: the ZeroMQ docs refer
+    to these as the 'proxy' and 'broker' models, although I like to think of
+    them as asymmetric and symmetric, respectively.
+
+    In the proxy model, the load balancer binds to an input socket and connects
+    to a number of output sockets downstream. This means no changes are
+    required to the servers that are being load-balanced, but the list of
+    servers is fixed; adding or removing servers from the cluster requires that
+    you reconfigure and restart the load balancer.
+
+    To create a proxy load balancer, pass a list of addresses as the `output`
+    argument:
+
+        >>> loadbal = LoadBalancer('inproc://zrpc',
+        ...     ('inproc://worker0', 'inproc://worker1', ...))
+        >>> loadbal.run()
+
+    In the broker model, the load balancer binds to two sockets; one for input
+    and one for output. This means the ZRPC servers now have to connect into
+    the load balancer instead, but they can come and go dynamically without
+    restarting the load balancer.
+
+    To create a broker load balancer, pass a single address as the `output`
+    argument:
+
+        >>> loadbal = LoadBalancer('inproc://zrpc', 'inproc://zrpc-workers')
+        >>> loadbal.run()
+    """
+
+    def __init__(self, input, output, context=None):
+        self.context = context or zmq.Context.instance()
+        self.input = input
+        self.output = output
+
+    def run(self, setup_callback=None):
+        logger.debug("Listening for requests on {0!r}", self.input)
+        input_socket = self.context.socket(zmq.XREP)
+        input_socket.bind(self.input)
+
+        output_socket = self.context.socket(zmq.XREQ)
+        if hasattr(self.output, '__iter__'):
+            logger.debug("Connecting to {0} workers", len(self.output))
+            for node in self.output:
+                output_socket.connect(node)
+        else:
+            logger.debug("Listening for workers on {0!r}", self.output)
+            output_socket.bind(self.output)
+
+        if setup_callback:
+            setup_callback(input_socket, output_socket)
+
+        try:
+            zmq.device(zmq.QUEUE, input_socket, output_socket)
+        finally:
+            input_socket.close()
+            output_socket.close()
---
+++ tests/loadbal_test.py
@@ -0,0 +1,89 @@
+from contextlib import contextmanager
+from Queue import Queue
+import threading
+
+import zmq
+
+from zrpc.loadbal import LoadBalancer
+
+
+@contextmanager
+def temporary_context():
+    context = zmq.Context.instance()
+    try:
+        yield context
+    finally:
+        context.term()
+
+
+@contextmanager
+def run_loadbal(input, output, context=None):
+    context = context or zmq.Context.instance()
+
+    # Set up a callback so we can be notified when the device connects/binds
+    # its input and output sockets. This resolves issues with attempting to
+    # connect to `inproc:` sockets before they've been bound at the other end.
+    queue = Queue()
+    callback = lambda *args: queue.put(args)
+
+    # Run the load balancer device in a separate thread. This will die once
+    # all its sockets are closed and the context terminated.
+    loadbal = LoadBalancer(input, output, context=context)
+    loadbal_thread = threading.Thread(target=loadbal.run,
+                                      kwargs={'setup_callback': callback})
+    loadbal_thread.daemon = True
+    loadbal_thread.start()
+
+    in_sock, out_sock = queue.get()
+    client = context.socket(zmq.REQ)
+    client.connect(input)
+
+    try:
+        yield client
+    finally:
+        client.close()
+        in_sock.close()
+        out_sock.close()
+
+
+@contextmanager
+def run_worker(bind=None, connect=None, context=None):
+    context = context or zmq.Context.instance()
+    worker = context.socket(zmq.REP)
+    if connect:
+        worker.connect(connect)
+    elif bind is None:
+        raise ValueError("Must specify either `bind` or `connect`")
+    else:
+        worker.bind(bind)
+
+    try:
+        yield worker
+    finally:
+        worker.close()
+
+
+def test_proxy_forwards_requests_to_specified_backends():
+    with temporary_context():
+        with run_worker(bind='inproc://worker0') as worker:
+            with run_loadbal('inproc://zrpc', ('inproc://worker0',)) as client:
+                request = {"id": "abc", "method": "add", "params": [3, 4]}
+                response = {"id": "abc", "error": None, "result": 7}
+
+                client.send_json(request)
+                assert worker.recv_json() == request
+                worker.send_json(response)
+                assert client.recv_json() == response
+
+
+def test_broker_forwards_requests_to_any_connected_backend():
+    with temporary_context():
+        with run_loadbal('inproc://zrpc-in', 'inproc://zrpc-out') as client:
+            with run_worker(connect='inproc://zrpc-out') as worker:
+                request = {"id": "abc", "method": "add", "params": [3, 4]}
+                response = {"id": "abc", "error": None, "result": 7}
+
+                client.send_json(request)
+                assert worker.recv_json() == request
+                worker.send_json(response)
+                assert client.recv_json() == response
