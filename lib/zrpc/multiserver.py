a8957a5b656cfd0b4cbce7cacee3114d0390a819
Zachary Voase <z@zacharyvoase.com>
2012-01-05 08:33:41 +0000

    Added a basic MultiServer implementation.

---
+++ lib/zrpc/multiserver.py
@@ -0,0 +1,76 @@
+from contextlib import closing, nested
+from Queue import Queue
+import threading
+import uuid
+
+import logbook
+import zmq
+
+from zrpc.concurrency import Callback, DummyCallback
+from zrpc.loadbal import LoadBalancer
+from zrpc.server import Server
+
+
+logger = logbook.Logger('zrpc.multiserver')
+
+
+class MultiServer(object):
+
+    """A multi-threaded ZMQ server."""
+
+    def __init__(self, addr, registry, connect=False, context=None):
+        self.context = context or zmq.Context.instance()
+        self.addr = addr
+        self.connect = connect
+        self.registry = registry
+
+    def run_device(self, daemon=True, callback=DummyCallback()):
+
+        """
+        Run the ZMQ queue device in a background thread.
+
+        The return value of this function will be the address of the device's
+        output socket (an `inproc://`-type address). In order to kill the
+        background thread, you need to terminate the context the device is
+        attached to.
+        """
+
+        output_addr = 'inproc://%s' % uuid.uuid4()
+        loadbal = LoadBalancer(self.addr, output_addr, context=self.context)
+
+        loadbal_thread = threading.Thread(target=loadbal.run,
+                                          kwargs={'callback': callback})
+        loadbal_thread.daemon = daemon
+        loadbal_thread.start()
+
+        return output_addr
+
+    def run(self, n_workers, callback=DummyCallback(), daemon=True):
+        loadbal_callback = type(callback)()
+        loadbal_addr = self.run_device(callback=loadbal_callback,
+                                       daemon=daemon)
+        # We need the load balancer to be bound before continuing.
+        loadbal_callback.wait() 
+
+        with callback.catch_exceptions():
+            server = Server(loadbal_addr, self.registry, connect=True,
+                            context=self.context)
+            server_threads = []
+            server_callbacks = []
+
+            for i in xrange(n_workers):
+                server_callback = type(callback)()
+                server_thread = threading.Thread(
+                    target=server.run, kwargs={'callback': server_callback})
+                server_thread.daemon = daemon
+                server_thread.start()
+                server_threads.append(server_thread)
+                server_callbacks.append(server_callback)
+
+            # Wait for all callbacks to complete before triggering ours.
+            server_sockets = [server_callback.wait()
+                            for server_callback in server_callbacks]
+
+        callback.send(server_sockets)
+
+        return loadbal_addr, server_threads
---
+++ tests/multiserver_test.py
@@ -0,0 +1,54 @@
+from __future__ import with_statement
+
+from contextlib import contextmanager
+import threading
+
+from bson import BSON
+from nose.tools import assert_equal
+import zmq
+
+from zrpc.concurrency import Callback
+from zrpc.server import Registry
+from zrpc.multiserver import MultiServer
+
+
+REGISTRY = Registry()
+
+@REGISTRY.method
+def add(x, y):
+    return x + y
+
+
+@contextmanager
+def multiserver_and_client(address, registry, n_workers):
+    context = zmq.Context.instance()
+
+    try:
+        cb = Callback()
+        ms = MultiServer(address, registry, context=context)
+        ms_thread = threading.Thread(target=ms.run,
+                                    args=(n_workers,),
+                                    kwargs={'callback': cb})
+        ms_thread.daemon = True
+        ms_thread.start()
+
+        cb.wait()
+        client = context.socket(zmq.REQ)
+        client.connect(address)
+
+        try:
+            yield client
+        finally:
+            client.close()
+    finally:
+        context.term()
+
+
+def test_server_responds_correctly():
+    with multiserver_and_client('inproc://zrpc', REGISTRY, 4) as client:
+        client.send(BSON.encode({
+            "id": "abc",
+            "method": "add",
+            "params": [3, 4]}))
+        assert_equal(BSON(client.recv()).decode(),
+                     {"id": "abc", "result": 7, "error": None})
